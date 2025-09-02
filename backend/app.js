61096c83eb69418650a40e2f21144b3985375776
Vardhan Jain <vardhanjaindit@gmail.com>
2025-09-03 00:48:53 +0530

    Add CORS validation in the backend

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,20 @@ const app = express();
 // Parse JSON bodies
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE'
+  );
+  next();
+});
+
+
 // Mount routes
 app.use('/api/places', placesRoutes);
 app.use('/api/users', userRoutes);
