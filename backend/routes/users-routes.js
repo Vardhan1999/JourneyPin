41a7541b54444a1436cc1f2c0c688b5461c6bd58
Vardhan Jain <vardhanjaindit@gmail.com>
2025-09-05 12:00:00 +0530

    Setup file upload middleware with Multer for handling image uploads

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -3,15 +3,17 @@ const router = express.Router();
 const { check } = require('express-validator');
 
 const usersController = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 router.get('/', usersController.getUsers);
 
 router.post(
   '/signup',
+  fileUpload.single('image'),
   [
     check('name').not().isEmpty(),
     check('email').normalizeEmail().isEmail(),
-    check('password').isLength({ min: 6 })
+    check('password').isLength({ min: 6 }),
   ],
   usersController.signup
 );
